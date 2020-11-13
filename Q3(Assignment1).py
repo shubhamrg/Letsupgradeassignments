df19d3589106c524cb2cb2dc2eaa773b82f0a4ee
shubhamrg <47100775+shubhamrg@users.noreply.github.com>
2020-11-13 18:46:02 +0530

    Add files via upload

---
+++ Q3(Assignment1).py
@@ -0,0 +1,10 @@
+x1 =int( input("Enter selling price:"))
+x2 = int(input("Enter cost price:"))
+
+if x2 < x1:
+           print("Profit")
+elif x1 < x2 :
+           print("Loss")
+
+else:
+    print("Neither")
